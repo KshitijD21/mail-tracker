a8f89bb95a6fd0f068c59dc5eb64d938527194ea
KshitijD21 <kshitijdumbre21@gmail.com>
2025-06-03 20:23:47 -0700

    fix: :bug: fixed tracking_link structure

--- src/main/java/com/example/mail_tracker/repository/TrackingLinkRepository.java
+++ src/main/java/com/example/mail_tracker/repository/TrackingLinkRepository.java
@@ -4,8 +4,10 @@ package com.example.mail_tracker.repository;
 import com.example.mail_tracker.entities.TrackingLinkEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 
 public interface TrackingLinkRepository extends MongoRepository<TrackingLinkEntity, String> {
 
-
+    Optional<TrackingLinkEntity> findByCode(String code);
 }
